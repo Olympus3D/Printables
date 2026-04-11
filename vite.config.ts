637753f1577fe5f49977b2f3dc6df6602fa1aa5c
Luis Guilherme Passos Ramos <20222tadssaj0015@ifba.edu.br>
2026-04-10 22:53:34 -0300

    aaa

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Printable-Apolo/', // Precisa ser exatamente igual ao repositório
+  base: '/Printables/', // Precisa ser exatamente igual ao repositório
 })
